4905eb6f9d9aa89a80f4ed6cdff8b2b4d360ff9e
s5065424 <75672621+s5065424@users.noreply.github.com>
2020-12-09 14:52:11 +0530

    Update arp1.py

--- arp1.py
+++ arp1.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import multiprocessing
 import time
 import datetime
 import random
@@ -10,12 +11,13 @@ sums = a+b
 seconds =[]
 values=[]
 timestamp = []
+rand = []
 a = time.monotonic()
 # For creating time stamp in .xlsx file 
-while(1):
+while 1:
     b = time.monotonic()
     ct = str(datetime.datetime.now())
-    ct = ct[0:19]
+    #ct = ct[0:19]
     #For creating random letters.
     ch = random.choice(string.ascii_letters)
     if((ch>='a' and ch<= 'y') or (ch>='A' and ch<='Y')):
@@ -23,11 +25,11 @@ while(1):
         seconds.append(str(b-a)[0:1])
         values.append(sums)
         timestamp.append(ct)
+        rand.append(ch)
         print(sums)
 # User can change the letter.
     if(ch == 'z' or ch == 'Z'):
-        df = pd.DataFrame.from_dict({'TimeStamp':timestamp,'Duration': seconds,'Sum Value':values})
+        df = pd.DataFrame.from_dict({'TimeStamp':timestamp,'random letters':rand,'Sum Value increment of (a+b)':values})
         df.to_excel('exported.xlsx', header=True, index=False)
         quit()
     
-
